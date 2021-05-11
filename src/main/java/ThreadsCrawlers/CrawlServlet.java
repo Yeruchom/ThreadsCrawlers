c9000d97f5b021e106773f251d23c7f9eab373e6
Yeruchom <yeruchomheller@gmail.com>
2021-05-11 18:11:33 +0300

    handeled the threads joining

--- src/main/java/ThreadsCrawlers/CrawlServlet.java
+++ src/main/java/ThreadsCrawlers/CrawlServlet.java
@@ -8,25 +8,33 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "CrawlServlet", value = "/CrawlServlet")
 public class CrawlServlet extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        System.out.println("in crawl get");
+       response.setContentType("text/html");
+       PrintWriter out = response.getWriter();
+       out.println("this is the meanwhile result");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("in crawl post");
-//        if(request.getParameter("url") != null)
-//            System.out.println("param: " + request.getParameter("url"));
-//else
-//    System.out.println("its null");
-
 
         PrintWriter writer = response.getWriter();
-        writer.println("working" + request.getParameter("url").trim());
-        request.getRequestDispatcher("").include(request, response);
-        writer.close();
-        new Crawler(request.getParameter("url").trim(), 0).crawl();
 
+        Crawler mainThread = new Crawler(request.getParameter("url").trim(), 0);
+        mainThread.start();
+
+        request.getRequestDispatcher("Crawling.html").include(request, response);
+
+        try {
+            mainThread.join();
+            request.getRequestDispatcher("CrawlingResults.html").include(request, response);
+        }
+        catch (InterruptedException i){System.out.println("exception on joining main thread");}
+        catch(IOException e){System.err.println("CrawlingResults.html file not found");};
+
+        writer.close();
     }
 }
--- src/main/java/ThreadsCrawlers/Crawler.java
+++ src/main/java/ThreadsCrawlers/Crawler.java
@@ -1,15 +1,24 @@
 package ThreadsCrawlers;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+//import sun.jvm.hotspot.runtime.Thread;
+import java.lang.Thread;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Crawler {
+public class Crawler extends Thread {
+
     private static HashSet<String> hashSet = new HashSet<String>();
+    private static AtomicInteger imgNum = new AtomicInteger();
     private static final Integer depth = 2;
+
+    private LinkedList<Thread> threads = new LinkedList<Thread>();
 
     private Integer mydepth;
     private String myurl;
@@ -17,22 +26,44 @@ public class Crawler {
     public Crawler(String url, Integer depth) {
         mydepth = depth;
         myurl = url;
-        System.out.println("url in constructor: " + myurl + "   depth = " + mydepth);
+//        threadsNum.addAndGet(1);//a new thread was created
     }
 
-    public void crawl(){
-        if (hashSet.contains(myurl) || mydepth >= depth)
-            return;
-        try {
-            hashSet.add(myurl);
+    public void run(){
+        System.out.println("url: " + myurl + " depth = " + mydepth + " i am: "+ Thread.currentThread().getId());
 
-            Elements links = Jsoup.connect(myurl).get().select("a[href]");
-//            System.out.println("url after connecting: " + myurl);
+
+        try{
+            hashSet.add(myurl);//for the first thread
+
+            Document doc = Jsoup.connect(myurl).get();
+            countImgs(doc);
+
+            Elements links = doc.select("a[href]");
             for (Element link : links) {
                 String url = link.absUrl("href");
-                if(!hashSet.contains(url))
-                    new Crawler(url, mydepth + 1).crawl();
+                if(mydepth < depth && add2hash(url))
+                {
+                    threads.add(new Crawler(url, mydepth + 1));
+                    threads.getLast().start(); }
             };
         }catch (IOException e) {System.out.println("an error happened on connecting to url");}
+//
+
+        for(Thread thread : threads){
+           try {
+               thread.join();
+           }catch (InterruptedException i){}
+        }
+    }
+
+    private static synchronized boolean add2hash(String url){
+        return hashSet.add(url);
+    }
+
+    private void countImgs(Document doc){
+        int num = doc.select("img").size();
+        imgNum.addAndGet(num);
+        System.out.println(Thread.currentThread().getId() + "adding " + num + "total = " +imgNum.get() );
     }
 }
